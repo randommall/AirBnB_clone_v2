a9dcc31007ecf73f20205500b6071e15614cf741
randommall <randommallstudio@gmail.com>
2024-04-17 16:41:25 +0100

    refactored scripts to continue from task 6

--- models/__init__.py
+++ models/__init__.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
-"""
-Instantiates a storage object.
-
--> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
-   instantiates a database storage engine (DBStorage).
--> Otherwise, instantiates a file storage engine (FileStorage).
-"""
-from os import getenv
+"""This module instantiates an object of class FileStorage"""
+from models.engine.file_storage import FileStorage
 
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
+storage = FileStorage()
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,12 +1,9 @@
+
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Column, ForeignKey, String
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+    name = ""
